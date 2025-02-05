1b73ab4878db98956ff7938444356bd02ca8d359
Ksenia Burgucheva <kburgucheva@Ksenias-Air.fritz.box>
2025-02-05 16:54:52 +0100

    Welcome page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
 export default nextConfig;
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+        port: "",
+        search: "",
+      },
+    ],
+  },
+};
